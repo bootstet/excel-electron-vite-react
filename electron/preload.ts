6f524dfb3858d873c5f30bd1c5f4818aadb204a4
bootstet@163.com <bootstet@163.com>
2024-10-16 10:59:03 +0800

    feat: electron build

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,32 +1,52 @@
 import { ipcRenderer, contextBridge } from 'electron'
 import path from 'node:path'
+import { electronAPI } from '@electron-toolkit/preload'
 
+
+const api = {}
 // --------- Expose some API to the Renderer process ---------
-contextBridge.exposeInMainWorld('ipcRenderer', {
-  on(...args: Parameters<typeof ipcRenderer.on>) {
-    const [channel, listener] = args
-    return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
-  },
-  off(...args: Parameters<typeof ipcRenderer.off>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.off(channel, ...omit)
-  },
-  send(...args: Parameters<typeof ipcRenderer.send>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.send(channel, ...omit)
-  },
-  invoke(...args: Parameters<typeof ipcRenderer.invoke>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.invoke(channel, ...omit)
-  },
+// contextBridge.exposeInMainWorld('ipcRenderer', {
+//   on(...args: Parameters<typeof ipcRenderer.on>) {
+//     const [channel, listener] = args
+//     return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
+//   },
+//   off(...args: Parameters<typeof ipcRenderer.off>) {
+//     const [channel, ...omit] = args
+//     return ipcRenderer.off(channel, ...omit)
+//   },
+//   send(...args: Parameters<typeof ipcRenderer.send>) {
+//     const [channel, ...omit] = args
+//     return ipcRenderer.send(channel, ...omit)
+//   },
+//   invoke(...args: Parameters<typeof ipcRenderer.invoke>) {
+//     const [channel, ...omit] = args
+//     return ipcRenderer.invoke(channel, ...omit)
+//   },
 
-  // You can expose other APTs you need here.
-  // ...
-})
+//   // You can expose other APTs you need here.
+//   // ...
+// })
 
-const childProcess = require('child_process')
-const exec = childProcess.exec
-let openExec
+if (process.contextIsolated) {
+  console.log(1)
+  try {
+    contextBridge.exposeInMainWorld('electron', electronAPI)
+    contextBridge.exposeInMainWorld('api', api)
+  } catch (error) {
+    console.error(error)
+  }
+} else {
+  console.log(2)
+  // @ts-ignore (define in dts)
+  window.electron = electronAPI
+  // @ts-ignore (define in dts)
+  window.api = api
+}
+
+
+
+
+
 
 // openExec = exec(`node ${path.join(process.env.APP_ROOT, 'service')}`, function(error: any, stdout: any, stderr: any) {
 //   if (error) {
